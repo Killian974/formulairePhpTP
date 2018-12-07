04eb6d0e53886447dfb2d84809e1f99d44402e58
Bufaloo <killian-r@live.fr>
2018-12-07 10:57:30 +0100

    Formulaire d'inscription et bdd ainsi que le html et css du formulaire a jour

--- bdd.php
+++ bdd.php
@@ -3,7 +3,7 @@
 try{
 $host ='localhost';
 $port = "3306";
-$db = "contact";
+$db = "utilisateurs";
 
     $bdd = new PDO("mysql:host=$host;port=$port;dbname=$db",'root','');
     echo "Vous etes à présent connecté à la bdd";
